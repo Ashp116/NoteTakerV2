ce92ea04be20303be0e9ab13e4aa2a7bfa78794c
Ashp116 <42877289+Ashp116@users.noreply.github.com>
2022-09-13 08:16:16 -0400

    Include out folder and tinyMCE

---
+++ out/main.js
@@ -0,0 +1,218 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var electron_1 = require("electron");
+var electron = require("electron");
+var exporter_1 = require("./src/exporter");
+var fs_1 = require("fs");
+var fs = require("fs");
+var IPCMain = electron.ipcMain;
+var path = require('path');
+var currentNoteFilePath;
+function createWindow() {
+    return __awaiter(this, void 0, void 0, function () {
+        var win, fileContents, fileJSON;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    win = new electron_1.BrowserWindow({
+                        minWidth: 800,
+                        minHeight: 600,
+                        webPreferences: {
+                            preload: path.join(__dirname, 'preload.js'),
+                            nodeIntegration: true,
+                            contextIsolation: false,
+                            // devTools: false,
+                        }
+                    });
+                    win.menuBarVisible = false;
+                    return [4 /*yield*/, win.loadURL(path.join(__dirname, "/views/index.html"))];
+                case 1:
+                    _a.sent();
+                    if (process.argv[1] !== ".") {
+                        fileContents = fs.readFileSync(process.argv[1]).toString();
+                        fileJSON = JSON.parse(fileContents);
+                        win.webContents.send("file-contents", fileJSON.content);
+                    }
+                    return [2 /*return*/, win];
+            }
+        });
+    });
+}
+electron_1.app.whenReady().then(function () { return __awaiter(void 0, void 0, void 0, function () {
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0: return [4 /*yield*/, createWindow()];
+            case 1:
+                _a.sent();
+                electron_1.app.on('activate', function () {
+                    createWindow();
+                });
+                return [2 /*return*/];
+        }
+    });
+}); });
+electron_1.app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        electron_1.app.quit();
+    }
+});
+IPCMain.on("export_pdf", function (event, args) { return __awaiter(void 0, void 0, void 0, function () {
+    var options;
+    return __generator(this, function (_a) {
+        options = {
+            //Placeholder 3
+            filters: [
+                { name: 'PDF', extensions: ['pdf'] },
+                { name: 'All Files', extensions: ['*'] }
+            ]
+        };
+        electron_1.dialog.showSaveDialog(electron_1.BrowserWindow.getFocusedWindow(), options)
+            .then(function (value) { return __awaiter(void 0, void 0, void 0, function () {
+            var _a, _b;
+            return __generator(this, function (_c) {
+                switch (_c.label) {
+                    case 0:
+                        if (!!value.canceled) return [3 /*break*/, 2];
+                        _a = fs_1.writeFileSync;
+                        _b = [value.filePath];
+                        return [4 /*yield*/, (0, exporter_1.export_pdf)(args)];
+                    case 1:
+                        _a.apply(void 0, _b.concat([_c.sent()]));
+                        _c.label = 2;
+                    case 2: return [2 /*return*/];
+                }
+            });
+        }); });
+        return [2 /*return*/];
+    });
+}); });
+IPCMain.on("export_docx", function (event, args) { return __awaiter(void 0, void 0, void 0, function () {
+    var options;
+    return __generator(this, function (_a) {
+        options = {
+            //Placeholder 3
+            filters: [
+                { name: 'Word Doc', extensions: ['docx'] },
+                { name: 'All Files', extensions: ['*'] }
+            ]
+        };
+        electron_1.dialog.showSaveDialog(electron_1.BrowserWindow.getFocusedWindow(), options)
+            .then(function (value) { return __awaiter(void 0, void 0, void 0, function () {
+            var _a, _b;
+            return __generator(this, function (_c) {
+                switch (_c.label) {
+                    case 0:
+                        if (!!value.canceled) return [3 /*break*/, 2];
+                        _a = fs_1.writeFileSync;
+                        _b = [value.filePath];
+                        return [4 /*yield*/, (0, exporter_1.export_docx)(args)];
+                    case 1:
+                        _a.apply(void 0, _b.concat([_c.sent()]));
+                        _c.label = 2;
+                    case 2: return [2 /*return*/];
+                }
+            });
+        }); })
+            .catch(function (reason) {
+            electron_1.dialog.showErrorBox("Error", reason.toString());
+        });
+        return [2 /*return*/];
+    });
+}); });
+IPCMain.on("save-note-file", function (event, args) {
+    var options = {
+        //Placeholder 3
+        filters: [
+            { name: 'Note', extensions: ['note'] },
+            { name: 'All Files', extensions: ['*'] }
+        ]
+    };
+    if (args.new_save || !currentNoteFilePath) {
+        electron_1.dialog.showSaveDialog(electron_1.BrowserWindow.getFocusedWindow(), options)
+            .then(function (value) { return __awaiter(void 0, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                if (!value.canceled) {
+                    currentNoteFilePath = value.filePath;
+                    (0, fs_1.writeFileSync)(value.filePath, JSON.stringify({
+                        content: args.content || ""
+                    }));
+                }
+                return [2 /*return*/];
+            });
+        }); })
+            .catch(function (reason) {
+            electron_1.dialog.showErrorBox("Error", reason.toString());
+        });
+    }
+    else {
+        (0, fs_1.writeFileSync)(currentNoteFilePath, JSON.stringify({
+            content: args.content || ""
+        }));
+    }
+});
+IPCMain.handle("icons", function (event, args) {
+    return require("./icons.json");
+});
+IPCMain.handle("open-note-file", function (event, args) {
+    var options = {
+        //Placeholder 3
+        filters: [
+            { name: 'Note', extensions: ['note'] },
+        ],
+        properties: [
+            "openFile",
+        ]
+    };
+    return electron_1.dialog.showOpenDialog(electron_1.BrowserWindow.getFocusedWindow(), options)
+        .then(function (value) { return __awaiter(void 0, void 0, void 0, function () {
+        var contents;
+        return __generator(this, function (_a) {
+            if (!value.canceled) {
+                currentNoteFilePath = value.filePaths[0];
+                contents = JSON.parse((0, fs_1.readFileSync)(value.filePaths[0]).toString());
+                return [2 /*return*/, (contents.content)];
+            }
+            return [2 /*return*/];
+        });
+    }); })
+        .catch(function (reason) {
+        electron_1.dialog.showErrorBox("Error", reason.toString());
+    });
+});
+//# sourceMappingURL=main.js.map
---
+++ out/views/renderer.js
@@ -0,0 +1,248 @@
+const {ipcRenderer} = require('electron')
+const {each} = require("tinymce");
+const ipc  = ipcRenderer
+
+let initFileContent = ""
+
+ipc.invoke("icons")
+    .then(json => {
+        Object.keys(json).forEach(key => {
+            tinymce.activeEditor.ui.registry.addIcon(key, json[key])
+        })
+    })
+
+let configTinyMCE = {
+    selector: 'textarea',
+    theme_advanced_resizing : false,
+    height: "97vh",
+    resize: false,
+    promotion: false,
+    plugins: [
+        "advlist", "anchor", "autolink", "charmap", "code", "fullscreen",
+        "help", "image", "insertdatetime", "link", "lists", "media",
+        "searchreplace", "table", "visualblocks", "slashcommands", "imagetools", 
+        "wordcount", "emoticons", "imagetools"
+    ],
+    toolbar: "undo redo | styles | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+    editor: "restore_draft",
+    toolbar_mode: 'floating',
+    automatic_uploads: true,
+    image_title: true,
+    menu: {
+        file: { title: 'File', items: 'newdocument open' },
+        custom: { title: 'Export', items: 'PDF  WordDocx' }
+    },
+    image_caption: true,
+    menubar: "file edit view insert format custom",
+    file_picker_callback: function (cb, value, meta) {
+        var input = document.createElement('input');
+        input.setAttribute('type', 'file');
+        input.setAttribute('accept', 'image/*');
+
+        /*
+          Note: In modern browsers input[type="file"] is functional without
+          even adding it to the DOM, but that might not be the case in some older
+          or quirky browsers like IE, so you might want to add it to the DOM
+          just in case, and visually hide it. And do not forget do remove it
+          once you do not need it anymore.
+        */
+
+        input.onchange = function () {
+            var file = this.files[0];
+
+            var reader = new FileReader();
+            reader.onload = function () {
+                /*
+                  Note: Now we need to register the blob in TinyMCEs image blob
+                  registry. In the next release this part hopefully won't be
+                  necessary, as we are looking to handle it internally.
+                */
+                var id = 'blobid' + (new Date()).getTime();
+                var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+                var base64 = reader.result.split(',')[1];
+                var blobInfo = blobCache.create(id, file, base64);
+                blobCache.add(blobInfo);
+
+                /* call the callback and populate the Title field with the file name */
+
+                cb(blobInfo.blobUri(), { title: file.name });
+            };
+
+            reader.readAsDataURL(file);
+
+
+        };
+
+        input.click();
+    },
+    setup: function(editor) {
+        editor.on("init", () => {
+            editor.setContent(initFileContent)
+
+            ipc.on("send-file-data", (event, args) => {
+                ipc.send("save-note-file", {
+                    new_save: false,
+                    content: editor.getContent()
+                })
+            })
+        })
+
+        editor.ui.registry.addMenuItem('PDF', {
+            text: 'PDF',
+            icon: "pdf",
+            onAction: function() {
+                ipc.send("export_pdf", tinymce.activeEditor.getContent())
+            }
+        });
+
+        editor.ui.registry.addMenuItem('open', {
+            text: 'Open Note',
+            onAction: function() {
+                ipc.invoke("open-note-file", "")
+                    .then(content => tinymce.activeEditor.setContent(content))
+            }
+        });
+
+        editor.ui.registry.addMenuItem('WordDocx_disabled', {
+            text: 'Docx',
+            icon: "word_doc",
+            onAction: function() {
+                ipc.send("export_docx", tinymce.activeEditor.getContent())
+            }
+        });
+
+        editor.on('keydown', function (event) {
+            if (event.code === "KeyS" && event.ctrlKey) {
+                console.log('KeyS Ctrl')
+                ipc.send("save-note-file", {
+                    new_save: false,
+                    content: tinymce.activeEditor.getContent()
+                })
+            }
+            if (event.code === "KeyS" && event.ctrlKey && event.shiftKey) {
+                console.log('KeyS Ctrl Shift')
+                ipc.send("save-note-file", {
+                    new_save: true,
+                    content: tinymce.activeEditor.getContent()
+                })
+            }
+        });
+    },
+    branding: false,
+
+}
+
+tinymce.PluginManager.add('slashcommands', function (editor) {
+    var insertActions = [
+        {
+            text: 'Heading 1',
+            icon: 'h1',
+            action: function () {
+                editor.execCommand('mceInsertContent', false, '<h1>Heading 1</h1>')
+                editor.selection.select(editor.selection.getNode());
+            }
+        },
+        {
+            text: 'Heading 2',
+            icon: 'h2',
+            action: function () {
+                editor.execCommand('mceInsertContent', false, '<h2>Heading 2</h2>');
+                editor.selection.select(editor.selection.getNode());
+            }
+        },
+        {
+            text: 'Heading 3',
+            icon: 'h3',
+            action: function () {
+                editor.execCommand('mceInsertContent', false, '<h3>Heading 3</h3>');
+                editor.selection.select(editor.selection.getNode());
+            }
+        },
+        {
+            type: 'separator'
+        },
+        {
+            text: 'Paragraph',
+            icon: 'p',
+            action: function () {
+                editor.execCommand('mceInsertContent', false, '<p>Paragraph</p>');
+                editor.selection.select(editor.selection.getNode());
+            }
+        },
+        {
+            type: 'separator'
+        },
+        {
+            text: 'Bulleted list',
+            icon: 'unordered-list',
+            action: function () {
+                editor.execCommand('InsertUnorderedList', false);
+            }
+        },
+        {
+            text: 'Numbered list',
+            icon: 'ordered-list',
+            action: function () {
+                editor.execCommand('InsertOrderedList', false);
+            }
+        }
+    ];
+
+    // Register the slash commands autocompleter
+    editor.ui.registry.addAutocompleter('slashcommands', {
+        ch: '/',
+        minChars: 0,
+        columns: 1,
+        fetch: function (pattern) {
+            const matchedActions = insertActions.filter(function (action) {
+                return action.type === 'separator' ||
+                    action.text.toLowerCase().indexOf(pattern.toLowerCase()) !== -1;
+            });
+
+            return new Promise(function (resolve) {
+                var results = matchedActions.map(function (action) {
+                    return {
+                        meta: action,
+                        text: action.text,
+                        icon: action.icon,
+                        value: action.text,
+                        type: action.type
+                    }
+                });
+                resolve(results);
+            });
+        },
+        onAction: function (autocompleteApi, rng, action, meta) {
+            editor.selection.setRng(rng);
+            // Some actions don't delete the "slash", so we delete all the slash
+            // command content before performing the action
+            editor.execCommand('Delete');
+            meta.action();
+            autocompleteApi.hide();
+        }
+    });
+    return {};
+});
+
+ipc.on("file-contents", (event, args) => {
+    console.log('file contented received')
+    initFileContent = args
+    tinymce.activeEditor.setContent(args)
+})
+
+tinyMCE.init(configTinyMCE);
+
+document.onkeydown = (event) => {
+    if (event.code === "KeyS" && event.ctrlKey) {
+        ipc.send("save-note-file", {
+            new_save: false,
+            content: tinymce.activeEditor.getContent()
+        })
+    }
+    if (event.code === "KeyS" && event.ctrlKey && event.shiftKey) {
+        ipc.send("save-note-file", {
+            new_save: true,
+            content: tinymce.activeEditor.getContent()
+        })
+    }
+}
